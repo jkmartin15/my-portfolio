0705222f95093a2ac840de045415a93b2e379cad
Your Name <johnkennethmartin.15@gmail.com>
2026-03-31 10:05:12 +0800

    Fix database configuration

--- blog/models.py
+++ blog/models.py
@@ -8,17 +8,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
-    from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
--- newpage/settings.py
+++ newpage/settings.py
@@ -2,14 +2,8 @@ import dj_database_url
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5@9t8^-2e+ap=081nj(#ix1-&&9lf-4z(6vdfpyy+nv6^z5c1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -61,19 +55,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'newpage.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
